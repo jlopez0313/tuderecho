2ed4e0c39b1d5937ea6b23437dcf5f9b92fa2f97
Jonathan Lopez <jlopez0313@hotmail.com>
2024-01-23 11:26:56 -0500

    tenant fixed

--- src/components/shared/SideMenu/SideMenu.jsx
+++ src/components/shared/SideMenu/SideMenu.jsx
@@ -33,7 +33,7 @@ export const SideMenu = ({animateClass, setAnimateClass}) => {
 
     const onSetLink = () => {
         if ( rol.toLowerCase() == 'profesional' ) {
-          setLink( '/profesionales' )
+          setLink( '' )
         } else {
           setLink( '/clientes' )
         }
