06e00ace8dfd8d57c05df8319c552edf7347b530
Jonathan Lopez <jlopez0313@hotmail.com>
2024-02-08 12:07:13 -0500

    welcome index

--- src/components/Admin/Index/IndexComponent.jsx
+++ src/components/Admin/Index/IndexComponent.jsx
@@ -1,8 +1,9 @@
+import Index from '@/assets/images/admin/index.jpg';
 
 export const IndexComponent = () => {
     return (
       <div className="w-100 p-4">
-        Welcome page
+        <img src={Index} />
       </div>
     )
 }
