c5ccd3112fad8b5ca4803902072b62f2ab12b6d5
Jonathan Lopez <jlopez0313@hotmail.com>
2024-02-21 00:51:46 -0500

    button add - added

--- src/components/shared/UpperMenu/UpperMenu.jsx
+++ src/components/shared/UpperMenu/UpperMenu.jsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Button from 'react-bootstrap/esm/Button';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faAdd, faPencil, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { Link, useNavigate } from 'react-router-dom';
 import { logout } from '@/helpers/helpers';
 import { decodeToken } from "react-jwt";
@@ -9,10 +9,18 @@ import { useTranslation } from 'react-i18next';
 import Popover from 'react-bootstrap/Popover';
 import styles from '@/assets/styles/shared.module.scss';
 import { getTenant } from "@/helpers/helpers";
+import { useState } from "react";
+import { ConferenciaModal } from '@/components/Modals/Conferencias/Conferencia';
+import { VideotecaModal } from '@/components/Modals/Videoteca/Videoteca';
+import { ComunidadesModal } from '@/components/Modals/Comunidades/Comunidades';
 
 export const UpperMenu = () => {
 
     const { t, i18n } = useTranslation();
+    
+    const [modalConferenciaShow, setModalConferenciaShow] = useState( false );
+    const [modalComunidadShow, setModalComunidadShow] = useState( false );
+    const [modalVideotecaShow, setModalVideotecaShow] = useState( false );
 
     const token = localStorage.getItem('token') || '';
     const { rol } = decodeToken(token);
@@ -25,6 +33,18 @@ export const UpperMenu = () => {
 
     const onLogout = () => {
         logout( navigate );
+    }
+
+    const onRefreshConferencias = ( doRefresh ) => {
+        setModalConferenciaShow(false)
+    }
+
+    const onRefreshVideoteca = (doRefresh) => {
+        setModalShow(false)
+    }
+
+    const onRefreshComunidades = (doRefresh) => {
+        setModalShow(false)
     }
 
     return (
@@ -40,30 +60,77 @@ export const UpperMenu = () => {
             <Popover.Body className={styles.popperBody}>
                 <ul className='mt-3'>
                     <strong className='text-danger d-block'> { t("conferencias.title") } </strong>
-                    <li className='border shadow-sm p-2 m-2'> 
-                        <Link to={'/conferencias'} className="">
-                            <FontAwesomeIcon className='me-2' icon={faSearch} />
-                            <strong className='text-dark'> { t("conferencias.discover") } </strong>
-                        </Link>
-                    </li>
+                        <li className='border shadow-sm p-2 m-2'> 
+                            <Link to={'/conferencias'} className="">
+                                <FontAwesomeIcon className='me-2' icon={faSearch} />
+                                <strong className='text-dark'> { t("conferencias.discover") } </strong>
+                            </Link>
+                        </li>
+
+                        <li className='border shadow-sm m-2'> 
+                            <div className="w-100 rounded p-2 bg-outlet-danger cursor-pointer"
+                                onClick={() => setModalConferenciaShow(true)}
+                            > 
+                                <FontAwesomeIcon icon={faPencil} />
+                                <strong> { t('conferencias.create') } </strong>
+                            </div>
+                        </li>
 
                     <strong className='text-danger d-block'> { t("videoteca.title") } </strong>
-                    <li className='border shadow-sm p-2 m-2'> 
-                        <Link to={'/videoteca'} className="">
-                            <FontAwesomeIcon className='me-2' icon={faSearch} />
-                            <strong className='text-dark'> { t("videoteca.discover") } </strong>
-                        </Link>
-                    </li>
+                        <li className='border shadow-sm p-2 m-2'> 
+                            <Link to={'/videoteca'} className="">
+                                <FontAwesomeIcon className='me-2' icon={faSearch} />
+                                <strong className='text-dark'> { t("videoteca.discover") } </strong>
+                            </Link>
+                        </li>
+                        
+
+                        <li className='border shadow-sm m-2'> 
+                            <div className="w-100 rounded p-2 bg-outlet-danger cursor-pointer"
+                                onClick={() => setModalVideotecaShow(true)}
+                            > 
+                                <FontAwesomeIcon icon={faPencil} />
+                                <strong> { t('videoteca.create') } </strong>
+                            </div>
+                        </li>
 
                     <strong className='text-danger d-block'> { t("comunidades.title") } </strong>
-                    <li className='border shadow-sm p-2 m-2'> 
-                        <Link to={'/comunidades'} className="">
-                            <FontAwesomeIcon className='me-2' icon={faSearch} />
-                            <strong className='text-dark'> { t("comunidades.discover") } </strong>
-                        </Link>
-                    </li>
+                        <li className='border shadow-sm p-2 m-2'> 
+                            <Link to={'/comunidades'} className="">
+                                <FontAwesomeIcon className='me-2' icon={faSearch} />
+                                <strong className='text-dark'> { t("comunidades.discover") } </strong>
+                            </Link>
+                        </li>
+                        
+
+                        <li className='border shadow-sm m-2'> 
+                            <div className="w-100 rounded p-2 bg-outlet-danger cursor-pointer"
+                                onClick={() => setModalVideotecaShow(true)}
+                            > 
+                                <FontAwesomeIcon icon={faPencil} />
+                                <strong> { t('comunidades.create') } </strong>
+                            </div>
+                        </li>
                 </ul>                        
             </Popover.Body>
+
+            <ConferenciaModal
+                title={ t('conferencias.create') }
+                modalShow={modalShow}
+                onHide={(doRefresh = false ) => onRefreshConferencias(doRefresh)}
+            />
+
+            <VideotecaModal
+                title={ t('videoteca.create') }
+                modalShow={modalShow}
+                onHide={(doRefresh = false) => onRefreshVideoteca(doRefresh)}
+            />
+
+            <ComunidadesModal
+                title={ t('comunidades.create') }
+                modalShow={modalShow}
+                onHide={(doRefresh = false) => onRefreshComunidades(doRefresh)}
+            />
 
             
         </div>
